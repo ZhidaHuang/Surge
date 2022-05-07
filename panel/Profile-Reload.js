6386fe0f0ee739ab8c0ad7c99692c64e70ef3d35
zD <73410050+ZhidaHuang@users.noreply.github.com>
2022-05-07 17:56:00 +0800

    Update Profile-Reload.js

--- panel/Profile-Reload.js
+++ panel/Profile-Reload.js
@@ -7,7 +7,7 @@ GitHub:https://github.com/mieqq/mieqq
 * 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
 
 * [Script]
-* 配置重载=script-path=https://raw.githubusercontent.com/ZhidaHuang/Surge/main/panel/Profile-Reload.js?token=GHSAT0AAAAAABUJ27HJ23OPQEZ2R6CVC3CUYTWIBDA,type=generic
+* 配置重载=script-path=https://raw.githubusercontent.com/ZhidaHuang/Surge/main/panel/Profile-Reload.js,type=generic
 */
 
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
