ac551abd26fff8e41c30ee787aa330013fba0ccb
zD (Stan) <73410050+ZhidaHuang@users.noreply.github.com>
2022-06-07 12:41:16 +0800

    Update Profile-Reload.js

--- panel/Profile-Reload.js
+++ panel/Profile-Reload.js
@@ -15,7 +15,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "配置重载",
         content: "配置重载成功",
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
+        icon: "ivfluid.bag",
         "icon-color": "#002FA7",
      })
     });
