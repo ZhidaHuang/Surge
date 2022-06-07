819901c31728a8d27a3a3c3bb516fa26838448dc
zD (Stan) <73410050+ZhidaHuang@users.noreply.github.com>
2022-06-07 12:06:35 +0800

    Update Profile-Reload.js

--- panel/Profile-Reload.js
+++ panel/Profile-Reload.js
@@ -15,7 +15,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "配置重载",
         content: "配置重载成功",
-        icon: "takeoutbag.and.cup.and.straw.fill",
+        icon: "takeoutbag.and.cup.and.straw",
         "icon-color": "#002FA7",
      })
     });
