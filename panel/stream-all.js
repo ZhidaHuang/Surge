a35566230db885c50c917e0546f3ce20a8d11421
zD (Stan) <73410050+ZhidaHuang@users.noreply.github.com>
2022-06-14 09:21:56 +0800

    Update stream-all.js

--- panel/stream-all.js
+++ panel/stream-all.js
@@ -33,7 +33,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
     let panel_result = {
       title: '流媒体解锁检测',
       content: '',
-      icon: 'lock.open.display',
+      icon: 'sparkles.tv',
       'icon-color': '#002FA7',
     }
   let [{ region, status }] = await Promise.all([testDisneyPlus()])
